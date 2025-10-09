a6b69e621f47fe26812c8e23126f8b0f86cae11e
Leozera2k <leofsousa98@gmail.com>
2025-10-09 14:50:32 -0300

    tabs funcionando

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { Header } from "@/components/Header";
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState("placar");
+
   return (
     <div className="">
+      <Header activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,27 @@
-export const Header = () => {
-    return 
-}
+"use client";
+export const Header = ({ activeTab, setActiveTab }) => {
+  return (
+    <header className="h-18 w-full bg-secondary flex items-center gap-4 justify-center">
+      <button
+        onClick={() => setActiveTab("placar")}
+        className={`w-48 text-bold text-2xl px-4 py-2 rounded-xl ${
+          activeTab === "placar"
+            ? "bg-primary text-white"
+            : "bg-accent text-secondary hover:bg-orange-100"
+        }`}
+      >
+        Placar
+      </button>
+      <button
+        onClick={() => setActiveTab("teams")}
+        className={`w-40text-bold text-2xl px-4 py-2 rounded-xl ${
+          activeTab === "teams"
+            ? "bg-primary text-white"
+            : "bg-accent text-secondary hover:bg-orange-100"
+        }`}
+      >
+        Adicionar Times
+      </button>
+    </header>
+  );
+};
